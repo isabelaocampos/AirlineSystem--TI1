866b65c42a2474d39c669484c5cf9c957560015e
Isabella <isabelaocampo08@gmail.com>
2023-04-30 00:31:56 -0500

    Added main completed

--- src/datastructures/ui/Main.java
+++ src/datastructures/ui/Main.java
@@ -1,24 +1,29 @@
-package ui;
-import java.util.Scanner;
+package datastructures.ui;
 
-public class Main {
+import java.util.Scanner;
+import datastructures.model.*;
+
+public class Main{
 
     private Scanner reader;
-   
-    
-    
+	private Controller controller;
     public Main(){
-        this.reader = new Scanner(System.in);
+        reader = new Scanner(System.in);
+		controller = new Controller();
         
     }
 
     public Scanner getReader(){
-        return reader;
+		return reader;
     }
+
+	public Controller getController(){
+		return controller;
+	}
     public static void main(String[] args) {
 
         Main main = new Main();
-
+		main.readerTxt();
         int option = 0; 
 
 				do{
@@ -32,48 +37,53 @@ public class Main {
 	//
 	public int getOptionShowMenu(){
 		int option = 0; 
-		System.out.println("<<<<< Welcome to Snakes and Ladders >>>>>");
+		System.out.println("<<<<< Welcome to Airline System >>>>>");
 		System.out.println(
-				"1. Play \n" +
+				"1. Arriving of passengers \n" +
+						"2. Boarding list of passengers \n" +
+						"3. Disembarking list of passengers \n" +
                 "0. Exit");
 
 		option = reader.nextInt(); 
 
 		return option; 
 	}
-    //
-    public int getOptionShowMenu2(){
-        int option2 = 0; 
-		System.out.println("<<<<< Time to play >>>>>");
-		System.out.println(
-				"1. Throw dice \n" +
-                "2. See snakes and ladders");
-		option2 = reader.nextInt(); 
-
-	    return option2;
-    }
-  
-
     public void executeOption(int option){
         
 
         switch(option){
-            case 1: 
-           
-            case 0:
-            System.out.println("Exit program.");
+			case 1:
+				System.out.println("Please write the pathfile of the file that has the passengers by order of arrival");
+				String file = reader.next();
+				controller.passengerOrder(file);
+				break;
+			case 2:
+				controller.showBoardingOrder();
+				break;
+
+			case 3:
+				controller.showDisOrder();
+				break;
+			case 0:
+				System.out.println("Exit program.");
         
             break;
             default:
-
+				System.out.println("Invalid option");
             break;
 
 
         }
     }
-    
 
+	public void readerTxt(){
+		System.out.println("Write the pathfile of the passenger file");
+		String pathfile = reader.next();
+		System.out.println("Write amount of passengers: ");
+		int num = reader.nextInt();
+		controller.uploadPss();
 
+	}
 
     public int validateIntegerInput(){
 		int option = 0; 
